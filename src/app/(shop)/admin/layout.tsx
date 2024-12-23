28e5414749441fcb3aab35fb24b519655834e1c0
Jose Tejada <jtejadavilca@gmail.com>
2024-12-23 19:53:58 +0000

    Adding guard layer in admin layout

---
+++ src/app/(shop)/admin/layout.tsx
@@ -0,0 +1,11 @@
+import { auth } from "@/auth.config";
+import { redirect } from "next/navigation";
+
+export default async function AdminLayout({ children }: { children: React.ReactNode }) {
+    const session = await auth();
+    if (!session || !session.user || session.user.role !== "ADMIN") {
+        return redirect("/");
+    }
+
+    return <>{children}</>;
+}
